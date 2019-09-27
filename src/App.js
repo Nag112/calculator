a9d815382becfad737795aef1f1ea244272f048f
Nag Charan <raju11297@gmail.com>
2019-09-27 15:48:49 +0530

    V0.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    output: "1234"
+  };
+  render() {
+    return (
+      <div className="calc-container">
+        <code>Nagacharan</code>
+        <div className="calc-body">
+        <div className="theme">
+       <p>nightmode</p>
+            <label class="switch">
+                <input type="checkbox" />
+                <span class="slider round"></span>
+              </label>
+       </div>
+          <div className="calc-output">{this.state.output}</div>
+      
+          <div className="calc-input">     
+          <button className="btn">Si-mod</button>
+          <button className="btn">Sign</button>     
+          <button className="btn">Sqrt</button>
+          <button className="btn">Square</button>          
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>/</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>X</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>-</button>
+            <button>C</button>
+            <button>0</button>
+            <button>=</button>
+            <button>+</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
