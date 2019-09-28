3b5e44d7603bcaf684d08dbf5161c9c4e6666e0a
Nag Charan <raju11297@gmail.com>
2019-09-28 11:13:39 +0530

    V1.0

--- src/redux/output/output.reducer.js
+++ src/redux/output/output.reducer.js
@@ -1,29 +1,27 @@
 const init_state = {
-    output:"",
-    input:"",
-    numStack:[],
-    symStack:[]
-}
-const outReducer=(state=init_state,action)=>
-{
-    switch(action.type)
-    {
-        case 'update':{
-            let temp = ""
-                return {...state,
-                     output:state.output+action.payload,
-                     input:state.input+action.payload
-                    }}
-        case 'push':
-            {
-                state.numStack.push(state.input)
-                return { ...state,
-                        input:""                    
-                }
-            }            
-        default :
-            return state
+  output: "",
+  input: "",
+  numStack: [],
+  symStack: []
+};
+const outReducer = (state = init_state, action) => {
+  switch (action.type) {
+    case "update": {
+      if (action.payload === "C") return init_state;
+      else
+        return {
+          ...state,
+          output: state.output + action.payload,
+          input: state.input + action.payload
+        };
     }
-}
+    case "push": {
+      state.numStack.push(state.input);
+      return { ...state, input: "" };
+    }
+    default:
+      return state;
+  }
+};
 
 export default outReducer;
